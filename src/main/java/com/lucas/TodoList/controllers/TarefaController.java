d23b9134ffbe02c7c09bb3ba57a5cb42906a43e3
Lucas <lucascastrosantos20007@gmail.com>
2025-05-25 22:47:40 -0300

    Feat: Função atualizar tarefa funcionando

--- src/main/java/com/lucas/TodoList/controllers/TarefaController.java
+++ src/main/java/com/lucas/TodoList/controllers/TarefaController.java
@@ -4,18 +4,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import com.lucas.TodoList.model.Tarefa;
-import com.lucas.TodoList.services.TarefaService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.lucas.TodoList.model.Tarefa;
+import com.lucas.TodoList.services.TarefaService;
 
 
 @RestController
@@ -27,11 +26,11 @@ public class TarefaController {
         this.tarefaService = tarefaService;
     }
 
-    // Endpoint para listar todas as tarefas
+   
     @GetMapping()
     public List<Tarefa> listarTarefas() {
         return tarefaService.listarTarefas();
-    } 
+    }
 
     // Endpoint para buscar uma tarefa por ID
     @GetMapping("/{id}")
@@ -40,25 +39,33 @@ public class TarefaController {
             .map(ResponseEntity::ok)
             .orElse(ResponseEntity.notFound().build());
     }
+
     // Endpoint para criar uma nova tarefa
     @PostMapping()
     public Tarefa criarTarefa(@RequestBody Tarefa tarefa) {
         return tarefaService.criarTarefa(tarefa);
-    // Endpoint para atualizar uma tarefa
     }
+
+    // ENDPOINT PARA ATUALIZAR UMA TAREFA (PUT) 
     @PutMapping("/{id}")
-    public ResponseEntity<Tarefa> atualizarTarefa(@PathVariable Long id, @RequestBody Tarefa tarefaAtualizada) {
-        Tarefa tarefaSalva = tarefaService.atualizarTarefa(id, tarefaAtualizada);
+    public ResponseEntity<Tarefa> atualizarTarefa(@PathVariable Long id, @RequestBody Tarefa tarefaComNovosDados) {
+        Tarefa tarefaSalva = tarefaService.atualizarTarefa(id, tarefaComNovosDados);
         if (tarefaSalva != null) {
             return ResponseEntity.ok(tarefaSalva);
         } else {
             return ResponseEntity.notFound().build();
         }
     }
+
     // Endpoint para deletar uma tarefa existente
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarTarefa(@PathVariable Long id) {
-        tarefaService.deletarTarefa(id);
-        return ResponseEntity.noContent().build();
+        Optional<Tarefa> tarefa = tarefaService.buscarTarefaPorId(id);
+        if (tarefa.isPresent()) {
+            tarefaService.deletarTarefa(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
